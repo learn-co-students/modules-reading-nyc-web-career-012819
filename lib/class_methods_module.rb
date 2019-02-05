6c0222139eb687d74758196ba2c2e02909a26b53
akramhelil <akramhelil@gmail.com>
2019-02-05 15:54:41 -0500

    Done.

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,8 @@
+module MetaDancing
+# Now it's the ClassMethods that nested in the FancyDance method.
+#Now the dance_module is include MetaDancing module as ClassMethods module
+  def metadata
+    "This class produces objects that love to dance."
+  end
+
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,19 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+
+class Kid
+  # include Dance
+  # extend MetaDancing
+
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+# #why dont we use two include for and one is FnacyDance-Instancemethods
+# #and another include for the ClassMethods?
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
